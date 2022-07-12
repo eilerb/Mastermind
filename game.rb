99c5305d3272f08311bcf4b01cc12192320faecc
eb <eiler.byberi@gmail.com>
2022-07-12 14:25:49 -0700

    Add the #play method

--- game.rb
+++ game.rb
@@ -2,11 +2,21 @@ require_relative 'cpu_player'
 require_relative 'human_player'
 
 class Game
-  @@code = []
+  attr_accessor :cpu, :human_player, :code
 
   def initialize
     @cpu = CPUPlayer.new
+    @human_player = nil
+    @code = []
+  end
+
+  def play
+    puts "Welcome to Mastermind!\nAll you have to do is guess the correct four color code!\nThe colors are red, green, blue, yellow, purple, and orange"
+    puts "\n"
     puts 'What is your name?'
     @human_player = HumanPlayer.new(gets.chomp)
   end
 end
+
+game = Game.new
+game.play
