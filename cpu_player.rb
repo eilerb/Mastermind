b6507e33276d04b1b15fde0a3568fed3a152ff72
eb <eiler.byberi@gmail.com>
2022-07-12 16:09:44 -0700

    Add the #make_code method

--- cpu_player.rb
+++ cpu_player.rb
@@ -1,14 +1,17 @@
 require_relative 'player'
 
 class CPUPlayer
-  attr_accessor :colors
+  attr_accessor :colors, :code
+
   def initialize
-    
+    @code = Array.new(4)
+    @colors = %w[red green blue yellow purple orange]
   end
 
   def make_code
     code = Array.new(4)
-    code = code.map { |item| item = colors.shuffle.pop}
+    self.code = code.map { |item| item = colors.shuffle.pop }
+  end
 end
 
 CPUPlayer < Player
