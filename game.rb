2836bb46be7e1c3c7be03f67286ae1743bd069ef
eb <eiler.byberi@gmail.com>
2022-07-12 17:36:52 -0700

    Add pry-byebug

--- game.rb
+++ game.rb
@@ -1,20 +1,24 @@
 require_relative 'cpu_player'
 require_relative 'human_player'
+require 'pry-byebug'
 
 class Game
   attr_accessor :cpu, :human_player, :code
 
   def initialize
+    puts "Welcome to Mastermind!\nAll you have to do is guess the correct four color code!\nThe colors are red, green, blue, yellow, purple, and orange"
+    puts "\n"
+    @human_player = HumanPlayer.new
     @cpu = CPUPlayer.new
-    @human_player = nil
-    @code = []
   end
 
   def play
-    puts "Welcome to Mastermind!\nAll you have to do is guess the correct four color code!\nThe colors are red, green, blue, yellow, purple, and orange"
-    puts "\n"
-    puts 'What is your name?'
-    @human_player = HumanPlayer.new(gets.chomp)
+    code = cpu.make_code
+    binding.pry
+    12.times do
+      break if human_player.crack_code(code)
+      human_player.player_guess
+    end
   end
 end
 
